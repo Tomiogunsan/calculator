b9b14d2c4611e16b2415d36f5b86263ab8c96ead
tomi ogunsan <tomiafolayan@gmail.com>
2022-09-22 13:54:26 -0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+let  display = document.getElementById('display')
+let buttons = Array.from(document.getElementsByClassName('button'))
+ 
+
+buttons.map(button => {
+  button.addEventListener('click', (e) =>{
+    console.log('click')
+    switch((e.target as HTMLButtonElement).innerText){
+      case 'AC':
+        display!.innerText = ''
+     
+        break;
+        case '=':
+          if(display)
+        display!.innerText = eval(display.innerText);
+          break;
+          case 'DEL':
+            if(display){
+              display!.innerText = display!.innerText.slice(0, -1);
+            }
+            break;
+        default:
+          display!.innerText += ((e.target as HTMLButtonElement).innerText)
+    
+      
+ } })
+})
